f1a5574a89c74bbe69bd6779262bddf37bf4672f
ANTH0RNY <nyaganthony@gmail.com>
2023-01-10 14:24:22 +0300

    first draft

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,18 @@
+#ifndef LIST_H
+#define LIST_H
+
+/**
+ * struct listint_t - sll
+ * @n: int
+ * @next: the next single linked list
+ * Description: single list list node
+ */
+typedef struct listint_t
+{
+	int n;
+	struct listint_t *next;
+} listint_t;
+
+int check_cycle(listint_t *list)
+
+#endif
