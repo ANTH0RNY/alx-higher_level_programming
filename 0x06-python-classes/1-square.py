1601e7906cc43eb28e14063841e0aa24aeca7354
ANTH0RNY <nyaganthony@gmail.com>
2023-01-24 12:49:10 +0300

    done upto 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' defines class square '''
+
+class square:
+    """Represents a square
+    Attributes:
+        __size (int): size of sides of the square
+    """
+    def __init__(self, size):
+        '''Init a square
+        Args:
+           size (int): size of the sides of a square
+        Returns: none
+        '''
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' defines class square '''
+
+class square:
+    """Represents a square
+    Attributes:
+        __size (int): size of sides of the square
+    """
+    def __init__(self, size = 0):
+        '''Init a square
+        Args:
+           size (int): size of the sides of a square
+        Returns: none
+        Raises:
+            TypeError: if the input is not an int
+            ValueError: if input is less than 0
+        '''
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+               self.__size = size
+        else:
+            raise TypeError('size must be an integer')
