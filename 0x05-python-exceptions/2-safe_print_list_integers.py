3d7b69a8023777f492f7322190b81254395e9b5e
ANTH0RNY <nyaganthony@gmail.com>
2023-01-25 21:16:37 +0300

    i think im done

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    length = 0
-
-    for i in range(x):
+    h = 0
+    for j in range(x):
         try:
-            print("{:d}".format(my_list[i]), end='')
-            length += 1
-        except (ValueError, TypeError):
+            print("{:d}".format(my_list[j]), end="")
+            h += 1
+        except(ValueError, TypeError):
             continue
     print()
-    return length
+    return h
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    length = []
-    for i in range(list_range):
+    le = []
+    for i in range(list_length):
         try:
             dv = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             dv = 0
         finally:
-            length.append(dv)
+            le.append(dv)
     return length
