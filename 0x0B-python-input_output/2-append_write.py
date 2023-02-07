6b75a2d7b55674b2d56621de2fbed05b81b4227f
ANTH0RNY <nyaganthony@gmail.com>
2023-02-07 18:39:52 +0300

    done advanced see you tommorow

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,5 +6,5 @@ Defines append_write
 
 def append_write(filename="", text=""):
     """functions that appends a string at end of file"""
-    with open(filename, "a", encode='utf-8') as file:
+    with open(filename, "a", encode='utf=8') as file:
         return file.write(text)
