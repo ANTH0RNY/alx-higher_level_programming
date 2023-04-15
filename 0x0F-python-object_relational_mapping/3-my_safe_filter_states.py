a68d50f15669353e722934bee886b27a529bcf77
ANTH0RNY <nyaganthony@gmail.com>
2023-04-15 08:09:32 +0300

    lets continue

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-
+"""
+Displays all values in the
+states from db
+"""
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
     """
     Connect to the db and get the states
-    from the db
+    from the db.
     """
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
     with db.cursor() as cur:
-        cur.execute("SELECT * \
-            FROM states \
-            WHERE name \
-            LIKE BINARY %(name)s \
-            ORDER BY states.id ASC", {'name': argv[4]})
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
 
         rows = cur.fetchall()
 
